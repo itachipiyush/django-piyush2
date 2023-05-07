5102b10ce869237b46b523e21953c26a7aeaf537
Piyush <piyushjha.2312@gmail.com>
2023-05-07 13:32:04 +0530

    initial commit

---
+++ temp1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+def base(request):
+    return render(request, 'C:/Users/HP/Desktop/piyush django/inherit1/templates/base.html', )
+
+def index(request):
+    return render(request, 'C:/Users/HP/Desktop/piyush django/inherit1/templates/index.html', )
+
+
